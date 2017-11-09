a217343c20abd97dea74519e2e21a72d40721e53
Manos Psychogyiopoulos <psyx@somewherewarm.net>
2017-11-09 22:46:42 +0200

    Rewrite script to utilize PB 5.6 filters

--- assets/js/wc-pb-bd-add-to-cart.js
+++ assets/js/wc-pb-bd-add-to-cart.js
@@ -1,84 +1,137 @@
 
-;( function ( $, window, document, undefined ) {
+;( function( $ ) {
 
-	$( '.bundle_data' )
+	var PB_Integration = function( bundle ) {
 
-	.on( 'woocommerce-product-bundle-initializing', function( event, composite ) {
+		/**
+		 * 'bundle_subtotals_data' filter callback.
+		 */
+		this.filter_bundle_subtotals_data = function( bundle_price_data, bundle, qty ) {
 
-	// 		composite.filters.add_filter( 'composite_price_html', function( price_html, view, price_data_array ) {
+			if ( bundle_price_data.applied_discount ) {
+				return bundle_price_data;
+			}
 
-	// 			// Avoid execution when in recursion.
-	// 			if ( typeof( price_data_array ) === 'undefined' || ( typeof( price_data_array ) !== 'undefined' && ! price_data_array.applied_discount ) ) {
+			var	quantities     = bundle_price_data.quantities,
+				discount_data  = bundle_price_data.bulk_discount_data.discount_array,
+				discount       = 0,
+				total_quantity = 0;
 
-	// 				var model                 = view.model,
-	// 					price_data            = model.price_data,
-	// 					total_quantity_object = model.price_data.quantities,
-	// 					global_discount_array = composite.settings.global_discount_array,
-	// 					discount 			  = 0,
-	// 					total_quantity        = 0;
+			if ( discount_data.length > 0 ) {
 
-	// 				// If the Quantity Discounts field is not empty.
-	// 				if ( global_discount_array.length > 0 ) {
+				// Sum quantities.
+				for ( var bundled_item_id in quantities ) {
+					total_quantity += quantities[ bundled_item_id ];
+				}
 
-	// 					// Sum quantity of each Component.
-	// 					for ( var component in total_quantity_object ) {
-	// 						total_quantity += total_quantity_object[ component ];
-	// 					}
+				// Check if there's a discount for that quantity.
+				for ( var i = 0; i < discount_data.length; i++ ) {
+					if ( typeof( discount_data[ i ].quantity_min ) !== 'undefined' && typeof( discount_data[ i ].quantity_max ) !== 'undefined' &&  typeof( discount_data[ i ].discount ) !== 'undefined' ) {
+						if ( total_quantity >= discount_data[ i ].quantity_min && ( total_quantity <= discount_data[ i ].quantity_max || '' === discount_data[ i ].quantity_max ) ) {
+							discount = discount_data[ i ].discount;
+						}
+					}
+				}
 
-	// 					// Check if there exists a discount for that quantity
-	// 					for ( var i = 0; i < global_discount_array.length; i++ ) {
-	// 						if ( total_quantity >= global_discount_array[ i ][ 'quantity_min' ] && total_quantity <= global_discount_array[ i ][ 'quantity_max' ] ) {
-	// 							discount = global_discount_array[ i ][ 'discount' ];
-	// 						}
-	// 					}
+				bundle_price_data.bulk_discount_data.discount = discount;
 
-	// 					if ( discount > 0 ) {
+				if ( discount > 0 ) {
 
-	// 						price_data = $.extend( true, {}, composite.data_model.price_data );
+					var price_data = $.extend( true, {}, bundle_price_data );
 
-	// 						$.each( composite.get_components(), function( index, component ) {
-	// 							price_data.prices[ component.component_id ] = price_data.prices[ component.component_id ] * ( 1 - discount / 100 );
-	// 						} );
+					$.each( bundle.bundled_items, function( index, bundled_item ) {
+						price_data.prices[ bundled_item.bundled_item_id ] = price_data.prices[ bundled_item.bundled_item_id ] * ( 1 - discount / 100 );
+					} );
 
-	// 						// Determine if discount should be applied to base price.
-	// 						if ( composite.settings.apply_discount_to_base ) {
-	// 							price_data.base_price = price_data.base_price * ( 1 - discount / 100 );
-	// 						}
+					// Determine if discount should be applied to the base price.
+					if ( 'yes' === bundle.price_data.bulk_discount_data.discount_base && price_data.base_price ) {
+						price_data.base_price = Number( price_data.base_price ) * ( 1 - discount / 100 );
+					}
 
-	// 						price_data_array = composite.data_model.calculate_subtotals( false, price_data );
+					price_data.applied_discount = true;
 
-	// 						var totals = composite.data_model.calculate_totals( price_data );
-	// 						price_data.totals = totals;
+					price_data = bundle.calculate_subtotals( false, price_data, qty );
+					price_data = bundle.calculate_totals( price_data );
 
-	// 						// Avoid displaying regular price crossed out.
-	// 						price_data.totals.regular_price = price_data.price;
-	// 						price_data.applied_discount     = true;
+					price_data.applied_discount = false;
 
-	// 						var discount_string        = '<span class="discount">' + wc_composite_params.i18n_bulk_discount + '</span>',
-	// 							discount_value         = '<span class="discount-amount">' + wc_composite_params.i18n_bulk_discount_value.replace( '%v', wc_cp_number_round( discount, 2 ) ) + '</span>',
-	// 							discount_html          = wc_composite_params.i18n_bulk_discount_format.replace( '%s', discount_string ).replace( '%v' , discount_value ),
-	// 							discount_html          = '<span class="price-discount">' + discount_html + '</span>',
-	// 							$price_html            = $( price_html ).wrapInner( '<span class="price-subtotal"></span>' ),
-	// 							$discounted_price_html = $( view.get_price_html( price_data ) ).wrapInner( '<span class="price-total"></span>' );
+					/*
+					 * Modify the totals.
+					 */
 
-	// 						// Modify existing "Total:" to "Subtotal:".
-	// 						$price_html.find( 'span.total' ).text( wc_composite_params.i18n_bulk_discount_subtotal );
+					bundle_price_data.base_price_totals = price_data.base_price_totals;
 
-	// 						// Append "Discount:" line.
-	// 						$price_html.append( discount_html );
+					$.each( bundle.bundled_items, function( index, bundled_item ) {
 
-	// 						// Append new "Total:" line.
-	// 						$price_html.append( $discounted_price_html.html() );
+						var bundled_item_totals = price_data[ 'bundled_item_' + bundled_item.bundled_item_id + '_totals' ];
 
-	// 						price_html = $price_html.prop( 'outerHTML' );
-	// 					}
-	// 				}
-	// 			}
+						if ( typeof bundled_item_totals !== 'undefined' ) {
+							bundle_price_data[ 'bundled_item_' + bundled_item.bundled_item_id + '_totals' ] = bundled_item_totals;
+						}
 
-	// 			return price_html;
+					} );
+				}
+			}
 
-	// 		}, 10, this );
+			return bundle_price_data;
+		};
 
-	 	} );
+		/**
+		 * 'bundle_total_price_html' filter callback.
+		 */
+		this.filter_bundle_total_price_html = function( price_html, bundle ) {
 
-} ) ( jQuery, window, document );
+			if ( bundle.price_data.bulk_discount_data.discount && bundle.price_data.subtotals.price !== bundle.price_data.totals.price ) {
+
+				var price_html_subtotals = '',
+					price_data_subtotals = $.extend( true, {}, bundle.price_data );
+
+				price_data_subtotals.totals = price_data_subtotals.subtotals;
+				price_html_subtotals        = bundle.get_price_html( price_data_subtotals );
+
+				var discount_string        = '<span class="discount">' + wc_bundle_params.i18n_bulk_discount + '</span>',
+					discount_value         = '<span class="discount-amount">' + wc_bundle_params.i18n_bulk_discount_value.replace( '%v', wc_pb_number_round( bundle.price_data.bulk_discount_data.discount, 2 ) ) + '</span>',
+					discount_html_inner    = wc_bundle_params.i18n_bulk_discount_format.replace( '%s', discount_string ).replace( '%v' , discount_value ),
+					discount_html          = '<span class="price-discount">' + discount_html_inner + '</span>',
+					$discounted_price_html = $( price_html ).wrapInner( '<span class="price-total"></span>' ),
+					$price_html            = $( price_html_subtotals ).wrapInner( '<span class="price-subtotal"></span>' );
+
+				// Modify existing "Total:" to "Subtotal:".
+				$price_html.find( 'span.total' ).text( wc_bundle_params.i18n_bulk_discount_subtotal );
+
+				// Append "Discount:" line.
+				$price_html.append( discount_html );
+
+				// Append new "Total:" line.
+				$price_html.append( $discounted_price_html.html() );
+
+				price_html = $price_html.prop( 'outerHTML' );
+			}
+
+			return price_html;
+		};
+
+		// Init.
+		this.initialize = function() {
+
+			/**
+			 * Filter totals using 'bundle_subtotals_data' JS filter.
+			 */
+			bundle.filters.add_filter( 'bundle_subtotals_data', this.filter_bundle_subtotals_data, 10, this );
+
+			/**
+			 * Filter price total html using 'bundle_total_price_html' JS filter.
+			 */
+			bundle.filters.add_filter( 'bundle_total_price_html', this.filter_bundle_total_price_html, 10, this );
+		};
+	};
+
+	// Hook into Bundles.
+	$( '.bundle_form .bundle_data' ).each( function() {
+		$( this ).on( 'woocommerce-product-bundle-initializing', function( event, bundle ) {
+			var pb_integration = new PB_Integration( bundle );
+			pb_integration.initialize();
+		} );
+	} );
+
+} ) ( jQuery );
